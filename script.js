e9d76a3402bc558dd7ea46cba1d70f7140d07271
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-01 11:21:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function displayTime() {
+  let now = new Date();
+  let time = now.toLocaleTimeString();
+  let date = now.toDateString();
+  document.getElementById("timer").innerHTML = `${time}, ${date}`;
+}
+
+setInterval(displayTime, 1000);
